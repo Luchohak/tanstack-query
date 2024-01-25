7a39bad8f8a58e4c99b74a7fb2c9f5dfc1ef9c90
Luchohak <a00958934@gmail.com>
2024-01-25 15:43:36 -0600

    Solve challenge to fetch event details and delete a desired event

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,44 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-import Header from '../Header.jsx';
+import Header from "../Header.jsx";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { deleteEvent, fetchEvent } from "../../util/http.js";
+import { queryClient } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 
 export default function EventDetails() {
+  const params = useParams();
+  const eventId = params.id;
+  const navigate = useNavigate();
+  let formattedDate = '';
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["event-detail", { eventId }],
+    queryFn: ({ signal }) => fetchEvent({ id: eventId, signal }),
+  });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
+  });
+
+  const handleDeleteEvent = () => {
+    mutate({ id: eventId });
+  };
+
+  if (data) {
+    let formattedDate = new Date(data.date).toLocaleDateString("en-US", {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +47,39 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+      {isPending && <LoadingIndicator />}
+      {data && (
+        <article id="event-details">
+          <header>
+            <h1>{data.title}</h1>
+            <nav>
+              <button onClick={handleDeleteEvent}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img
+              src={`http://localhost:3000/${data.image}`}
+              alt={data.description}
+            />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{data.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate ? formattedDate : data.date}</time>
+              </div>
+              <p id="event-details-description">{data.description}</p>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
           </div>
-        </div>
-      </article>
+        </article>
+      )}
+      {isError && (
+        <ErrorBlock
+          title="Failed to fetch event"
+          message={
+            error.info?.message || "Could not fetch event. Try again later"
+          }
+        />
+      )}
     </>
   );
 }
--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -20,7 +20,6 @@ export default function NewEvent() {
 
   function handleSubmit(formData) {
     mutate({ event: formData });
-    navigate("/events");
   }
 
   return (
